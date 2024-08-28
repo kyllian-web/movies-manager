77edbd1d1c72bbf7ab013fb754bcb862f95107ee
Kyllian <kyllian.guilbert.dev@gmail.com>
2024-08-28 20:54:12 +0200

    form submit without reloading + show movies list

--- app.js
+++ app.js
@@ -14,6 +14,32 @@ function recordData(){
     localStorage.setItem("kgd_movies_data", JSON.stringify(catalog));
 }
 
+function getData(){
+    let moviesList = localStorage.getItem("kgd_movies_data");
+    console.log(moviesList)
+    return JSON.parse(moviesList)
+}
 
-console.log(catalogue)
 
+console.log(catalog)
+
+
+function showElement() {
+    const newDiv = document.createElement("div");
+    
+    let moviesFromLocalStorage = getData()
+        for (const movie of moviesFromLocalStorage) {
+              console.log(movie);
+              // add the text node to the newly created div
+              const HTMLname = document.createTextNode("Nom du film : " + movie.name);
+              const HTMLnote = document.createElement('p');
+              HTMLnote.innerHTML = "Note du film : " + movie.note;
+              HTMLnote.classList.add("note");
+              newDiv.appendChild(HTMLname);
+              newDiv.appendChild(HTMLnote);
+    }
+  
+    const container = document.getElementById("container");
+    container.innerHTML = newDiv.innerHTML ;
+  }
+  
